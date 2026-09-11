447ba93ff62aae8966f33afb6a8cc9449d91316b
Kelvon Sango <kts7388@rit.edu>
2026-09-10 23:23:48 -0400

    Calculate average flow rate from volume and time

--- pump_flow_rate.py
+++ pump_flow_rate.py
@@ -1,1 +1,4 @@
-
+volume = float(input("Enter the volume transferred (L): "))
+elapsed_time = float(input("Enter the elapsed time (s): "))
+flow_rate = volume / elapsed_time
+print(f"Average flow rate: {flow_rate} L/s"
